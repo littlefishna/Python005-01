78879c517ffb0e560fd47067f4d3266ca56af206
Dan <tattonot@163.com>
2020-12-18 10:43:39 +0800

    修改粘包问题
    使用struct包

--- week02/homework_week02_1_client.py
+++ week02/homework_week02_1_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import struct
 
 targetHost = "192.168.109.129"
 targetPort = 6666
@@ -8,9 +9,30 @@ targetPort = 6666
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((targetHost,targetPort))
 
-with open("transforfile.txt","r+") as f:
-    for line in f.readlines():
-        clientSocket.send(line.encode('utf-8'))
+# with open("transforfile.txt","rb") as f: # 网络传输为 2进制数据流， rb 即可
+#     for line in f.readlines():
+#         # 由于传输的是文本，无法用特殊符号进行分割获取传输长度，如果 过大(极值) 怎么办
+#         packline = str(len(line)).zfill(10)
+#         clientSocket.send(packline.encode('utf-8')) # 先发送 长度
+#         clientSocket.send(line.encode('utf-8')) # 再发送 目标数据
+# clientSocket.send('quit.EOF'.encode('utf-8'))
 
-clientSocket.send('quit.EOF'.encode('utf-8'))
+# 先发送长度，再发送数据
+# while True:
+#     line = input('>')
+#     packline = str(len(line)).zfill(10)
+#     clientSocket.send(packline.encode('utf-8')) # 先发送 长度
+#     clientSocket.send(line.encode('utf-8')) # 再发送 目标数据
+
+while True:
+    line = input('>')
+    packheader = struct.pack("i",len(line)) # i 封装成 4字节(长度为4)的数据包
+    clientSocket.send(packheader) # 先发送 长度
+    clientSocket.send(line.encode('utf-8')) # 再发送 目标数据
+
+# data = "adsf"
+# results = struct.pack("i",len(data))
+# print(results,len(results))
+# print(struct.unpack("i",results)[0])
+
 
--- week02/homework_week02_1_server.py
+++ week02/homework_week02_1_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import struct
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,14 +15,22 @@ serverSocket.listen(1)
 while True:
     clientSocket,clientAddr = serverSocket.accept()
     file = open("results.log","a+")
+
+    # 先接收长度，在接受数据
+    # i=0
     while True:
-        data = clientSocket.recv(1024)
-        if not data:
+        header = clientSocket.recv(4)
+        # print(i,type(header), header)
+        # i += 1
+        if not header:
             break
-        elif data.decode('utf-8') == 'quit.EOF':
-            exit(1)
         else:
-            file.write(data.decode('utf-8'))
+            strlength = struct.unpack("i",header)
+            print(strlength[0])
+            # 再根据长度接收数据
+            # 可能遇到的 问题： 长度多大(极值条件),需要对长度进行判断，长度大于某个值，需要对数据进行循环读取写入
+            strData = clientSocket.recv(int(strlength[0]))
+            print(strData.decode('utf-8'))
 
     file.close()
     clientSocket.close()
